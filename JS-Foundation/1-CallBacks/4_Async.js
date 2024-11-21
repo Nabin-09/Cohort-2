6999aa8bf65de2eb846fa580eb516594f106aefb
Nabin <sharmanabin.0910@gmail.com>
2024-11-21 19:26:04 +0530

    Async Functions

--- JS-Foundation/1-CallBacks/4_Async.js
+++ JS-Foundation/1-CallBacks/4_Async.js
@@ -10,6 +10,13 @@ function findSumTill100(){
 }
 setTimeout(findSumTill100, 5000) //Calling an async Function
 console.log("Namaste");
+/* Asynchronous functions in JavaScript are a key feature that allows developers to write code that can perform long-running tasks without blocking the main execution thread. This capability is essential for maintaining responsiveness in applications, especially when dealing with operations like network requests, file reading, or timers. 
+Common async fucntions are : 
+setTimeout - The setTimeout() function in JavaScript is used to execute a specified function after a designated delay, measured in milliseconds. 
+fs.readFile - reads a file from your system
+Fetch - to fetch some data from API endpoint
+*/
+
 
 
 //11:46 1.5
