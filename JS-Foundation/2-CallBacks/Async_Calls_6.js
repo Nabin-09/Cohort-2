b238f1691f8817e26bddae1590347af38d2d6337
Nabin <sharmanabin.0910@gmail.com>
2025-01-27 20:43:43 +0530

    Pq-1

--- JS-Foundation/2-CallBacks/Async_Calls_6.js
+++ JS-Foundation/2-CallBacks/Async_Calls_6.js
@@ -2,5 +2,6 @@ console.log("Hi!");//This gets logged first
 function timeout(){
     console.log("Click me !");//Then this runs 
 }
-setTimeout(timeout, 5000);
+setTimeout(timeout, 5000);//Makes the timeout to wait for 5s
 console.log("Welcome to loupe .")//Second
+//If the JS thread is running and the async task is done it waits in the callback queue until the JS thread is idle.
---
+++ JS-Foundation/2-CallBacks/Async_Quiz_7.js
@@ -0,0 +1,11 @@
+function timeout(){
+    console.log("Click me !");
+}
+console.log("HI!");
+setTimeout(timeout , 1000);
+console.log("Welcome to loupe ! ")
+for(let c = 0 ; c < 100000000; c++){
+    c++;
+}
+console.log("Expensive operation done");
+//What runs when?
