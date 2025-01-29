5d1fd5d69753d0320fe12641813eb9a43903da12
Nabin <sharmanabin.0910@gmail.com>
2025-01-29 18:08:01 +0530

    Date Class

---
+++ Week-1/Promises/More_classes_3.js
@@ -0,0 +1,7 @@
+//This piece of code was written on 29th Jan , 2025 (Wednesday)
+const now = new Date();
+console.log(now)//Prints current time in UTC-0
+console.log(now.getDate());//29
+console.log(now.getDay());//3
+console.log(now.getFullYear());//2025
+console.log(now.getMonth());
