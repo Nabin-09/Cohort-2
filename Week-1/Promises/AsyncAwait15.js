e61254d9ae93c7b0d6f741720d90af8ca1b71efb
Nabin <sharmanabin.0910@gmail.com>
2025-01-30 23:16:02 +0530

    Async-Await

---
+++ Week-1/Promises/AsyncAwait15.js
@@ -0,0 +1,20 @@
+//Async Await is just syntactical sugar to make look async calls like sync ones
+function setTimeoutPromisifed(duration){
+    return new Promise(function(resolve){
+        setTimeout(resolve, duration);
+    });
+}
+async function solve(){//returns promise
+    await setTimeoutPromisifed(1000);
+    console.log("HI");
+    await setTimeoutPromisifed(3000);
+    console.log("HELLO");
+    await setTimeoutPromisifed(5000);
+    console.log("HI THERE");
+}
+solve();    
+/*
+HI
+HELLO
+HI THERE
+ */
