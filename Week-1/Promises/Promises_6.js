c3009c64bed276cc7e43d7e4d858611434b2a063
Nabin <sharmanabin.0910@gmail.com>
2025-01-30 20:52:47 +0530

    Promises-then

--- Week-1/Promises/Promises_6.js
+++ Week-1/Promises/Promises_6.js
@@ -5,3 +5,11 @@
     console.log("Main is called ! ")//called after 3s
  }
  waitFor3s(main);
+
+function setTimeoutPromsified(){
+     return new Promise(waitFor3s); 
+}
+function main(){
+    console.log("Main is called ")
+}
+setTimeoutPromsified(3000).then(main);
